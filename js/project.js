f3194b8f9818288d7f7fd869639042b48ac9be28
Michał Janczykowski <mjanczykowski@gmail.com>
2015-05-28 21:26:39 +0200

    simple fragment shader

--- js/project.js
+++ js/project.js
@@ -6,9 +6,13 @@ var vertexShader = [
     "",
     "varying float vAmount;",
     "varying vec2 vUV;",
+    "varying vec3 vPos;",
+    "varying vec3 vNormal;",
     "",
     "void main()",
     "{",
+    "    vPos = (modelMatrix * vec4(position, 1.0)).xyz;",
+    "    vNormal = normalMatrix * normal;",
     "    vUV = uv;",
     "    vec4 bumpData = texture2D( bumpTexture, uv );",
     "",
@@ -21,13 +25,33 @@ var vertexShader = [
     "}"
 ].join("\n");
 
-var fragmentShader = "void main() { gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); }";
+var fragmentShader = [
+    "uniform vec3 diffuse;",
+    "varying vec3 vPos;",
+    "varying vec3 vNormal;",
+    "uniform vec3 pointLightColor[MAX_POINT_LIGHTS];",
+    "uniform vec3 pointLightPosition[MAX_POINT_LIGHTS];",
+    "uniform float pointLightDistance[MAX_POINT_LIGHTS];",
+    "",
+    "void main()",
+    "{",
+    "   vec4 addedLights = vec4(0.0, 0.0, 0.0, 1.0);",
+    "   for(int l = 0; l < MAX_POINT_LIGHTS; l++) {",
+    "       vec3 lightDirection = normalize(vPos - pointLightPosition[l]);",
+    "       addedLights.rgb += clamp(dot(-lightDirection, vNormal), 0.0, 1.0) * pointLightColor[l];",
+    "   }",
+    "   gl_FragColor = mix(vec4(diffuse.x, diffuse.y, diffuse.z, 1.0), addedLights, addedLights);",
+    "}"
+].join("\n");
 
 var project = {
     init: function () {
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setClearColor(0xffffff);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.shadowMapEnabled = true;
+        this.renderer.autoClear = true;
+        this.renderer.autoClearColor = true;
 
         document.body.appendChild(this.renderer.domElement);
         window.addEventListener('resize', this.onWindowResize, false);
@@ -35,12 +59,13 @@ var project = {
         this.scene = new THREE.Scene();
 
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-        this.camera.position.set(0, 0, 100);
+        this.camX = 0;
+        this.camera.position.set(0, 20, 500);
         this.camera.lookAt(project.scene.position);
         this.scene.add(this.camera);
 
         var light = new THREE.PointLight(0xffffff);
-        light.position.set(100, 250, 100);
+        light.position.set(100, 200, 100);
         this.scene.add(light);
 
         // SKYBOX
@@ -57,10 +82,17 @@ var project = {
         var bumpScale = 200.0;
 
         // use "this." to create global object
-        var customUniforms = {
-            bumpTexture: {type: "t", value: bumpTexture},
-            bumpScale: {type: "f", value: bumpScale}
-        };
+        var customUniforms = THREE.UniformsUtils.merge(
+            [THREE.UniformsLib['lights'],
+                {
+                    bumpTexture: {type: "t", value: bumpTexture},
+                    bumpScale: {type: "f", value: bumpScale},
+                    diffuse: {type: 'c', value: new THREE.Color(0x00ff00)}
+                }
+            ]
+        );
+
+        customUniforms.bumpTexture = {type: "t", value: bumpTexture};
 
         // create custom material from the shader code above
         //   that is within specially labelled script tags
@@ -69,7 +101,8 @@ var project = {
                 uniforms: customUniforms,
                 vertexShader: vertexShader,
                 fragmentShader: fragmentShader,
-                side: THREE.DoubleSide
+                //side: THREE.DoubleSide,
+                lights: true
             });
 
         var planeGeo = new THREE.PlaneGeometry(1000, 1000, 100, 100);
@@ -89,6 +122,9 @@ var project = {
         project.render();
     },
     render: function () {
+        this.camX += 0.3;
+        this.camera.position.set(this.camX, 20, 500);
+        this.camera.lookAt(project.scene.position);
         this.renderer.render(this.scene, this.camera);
     }
 };
