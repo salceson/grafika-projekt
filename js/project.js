049dc34d423f71e05230bbc1923a856b35dfc178
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2015-06-09 18:26:18 +0200

    Removed cube.

--- js/project.js
+++ js/project.js
@@ -126,20 +126,6 @@ var project = {
         spotLight0.angle = Math.PI / 2;
         this.scene.add(spotLight0);
 
-        // create a cube
-        var cubeGeometry = new THREE.BoxGeometry(100, 100, 100);
-        var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff3333});
-        var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        cube.castShadow = true;
-
-        // position the cube
-        cube.position.x = -4;
-        cube.position.y = 200;
-        cube.position.z = 0;
-
-        // add the cube to the scene
-        this.scene.add(cube);
-
         this.loadSkyBox();
 
         this.loadShaderTerrain();
